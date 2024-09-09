3dbb1658e0d01080cff36d10a19104e36e6b8b6f
tearsofdespair <mealiv2004@gmail.com>
2024-09-09 14:38:37 +0300

    Added basic Controls with new input system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] public float Speed;
     [SerializeField] public float RotationSpeed;
-    public IRotationStrategy Strategy;
+    private Controls controls;
+
+    private void Awake()
+    {
+        controls = new Controls();
+    }
+
     private void Update()
     {
         this.transform.position += transform.right * Speed * Time.deltaTime;
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Strategy = new ActivatedRotationStrategy();
-        }
-        else
-        {
-            Strategy = new IdleRotationStrategy();
-        }
-
-        Strategy.Rotate(this.transform, RotationSpeed);
     }
 }
