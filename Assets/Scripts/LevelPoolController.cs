1f90f782d82019c9ca2ff4b8d84dfbfcd3380e72
tearsofdespair <mealiv2004@gmail.com>
2024-09-16 05:59:16 +0300

    Add some changes

--- Assets/Scripts/LevelPoolController.cs
+++ Assets/Scripts/LevelPoolController.cs
@@ -10,11 +10,13 @@ public class LevelPoolController : MonoBehaviour
     private GameObject _lastLevel;
     private GameObject _preLastLevel;
     private GameObject _newLevel;
+    [SerializeField] public Transform SpawnPosition;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("NextLevel"))
         {
             _newLevel = _poolService.Spawn(_levels[_random.Next(0, _levels.Count - 1)]);
+            _newLevel.transform.position = SpawnPosition.position;
             _poolService.Despawn(_lastLevel);
             _lastLevel = _preLastLevel;
             _preLastLevel = _newLevel;
